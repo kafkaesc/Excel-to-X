a51636b6b014edd2c41800cd3b21c640b96a7023
kafkaesc <jared.a.hettinger@gmail.com>
2024-12-10 18:39:27 -0600

    Setup to insert from XLSX

---
+++ 04-Insert-via-XLSX/README.md
@@ -0,0 +1,5 @@
+# Insert via XLSX
+
+Built by Jared Hettinger
+
+This unit contains a node project that can set up a local sqlite database. Once the database is ready, running the program will parse data from the `in.csv` file and insert values into the database.
---
+++ 04-Insert-via-XLSX/index.js
@@ -0,0 +1,22 @@
+console.log('Running Insert-via-XLSX...\n');
+
+const dbFilename = 'music.db';
+
+// Setup objects for sqlite access, file system, and the file reader
+const sq = require('sqlite3');
+const fs = require('fs');
+const fr = require('xlsx');
+const db = new sq.Database(dbFilename);
+const inFile = fr.readFile('./in.xlsx');
+const sheets = inFile.SheetNames;
+
+// Initiate empty errors array for logging any failed rows
+const errors = [];
+
+for (let i = 0; i < sheets.length; i++) {
+    const sheet = fr.utils.sheet_to_json(inFile.Sheets[inFile.SheetNames[i]]);
+    for (let j = 0; j < sheet.length; j++) {
+        // Uncomment below to log the data on each row
+        console.log(`${inFile.SheetNames[i]} Row ${j + 1}: `, sheet[j]);
+    }
+}
